e5aa29512e6ef4105d12f4f94dcfd426bc997a2b
xemlock <xemlock@gmail.com>
2015-10-20 15:57:05 +0200

    Fixed resolving modules from spinal-case named modules

--- library/ZeframMvc/Loader/ModuleAutoloader.php
+++ library/ZeframMvc/Loader/ModuleAutoloader.php
@@ -4,41 +4,72 @@ namespace ZeframMvc\Loader;
 
 use Zend\Loader\ModuleAutoloader as Zf2ModuleAutoloader;
 
+/**
+ * This module autoloader once instantiated can be accessed externally
+ * and can handle spinal-case namespaces.
+ */
 class ModuleAutoloader extends Zf2ModuleAutoloader
 {
     /**
-     * Autoload a module class
+     * Instance so that we can hook up to load module classes residing in spinal-case namespaces (ZF1)
+     * In PHP 5.4.24-5.4.45, 5.5.8+ autoloading is not performed for invalid namespaces, in
+     * 5.1.0-5.4.23 and 5.5.0-5.5.7 it is. To properly handle spinal-case namespaces we must
+     * be able to explicitly call autoload() from the outside - hence the getInstance() method.
      *
-     * @param   $class
-     * @return  mixed
-     *          False [if unable to load $class]
-     *          get_class($class) [if $class is successfully loaded]
+     * @var \ZeframMvc\Loader\ModuleAutoloader
      */
-    public function autoload($class)
+    protected static $_instance;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function __construct($options = null)
     {
-        if (false !== ($classLoaded = parent::autoload($class))) {
-            return $classLoaded;
-        }
+        parent::__construct($options);
+        self::$_instance = $this;
+    }
 
-        // Limit scope of this autoloader
-        if (substr($class, -7) !== '\Module') {
-            return false;
-        }
-
-        $moduleName = substr($class, 0, -7);
-
+    /**
+     * {@inheritDoc}
+     */
+    protected function loadModuleFromDir($dirPath, $class)
+    {
         // Module class may reside in a directory that uses spinal-case
         // naming style (convention used in ZF1). In order to handle such
-        // case convert CamelCase namespace to spinal-case and try to load
-        // module class using this (invalid) namespace. If file matching
-        // the namespace is found, it will be imported.
+        // case convert spinal-case (invalid) namespace to CamelCase and try to load
+        // module class using this namespace - dir path remains intact.
+        if (preg_match('/^[a-z][-.a-z]+\\\\/', $class)) {
+            list($moduleName, $moduleClass) = explode('\\', $class, 2);
+            $moduleName = self::formatModuleName($moduleName);
+            $class = $moduleName . '\\' . $moduleClass;
+        }
+        return parent::loadModuleFromDir($dirPath, $class);
+    }
 
-        $namespace = substr($moduleName, 0, $pos = strpos($moduleName, '\\'));
-        $namespace = strtolower(preg_replace('/([^A-Z])([A-Z])/', '$1-$2', $namespace));
 
-        $class = $namespace . substr($moduleName, $pos) . '\Module';
+    /**
+     * Format a module name
+     *
+     * @param  string $name
+     * @return string
+     */
+    public static function formatModuleName($name)
+    {
+        $name = strtolower($name);
+        $name = str_replace(array('-', '.'), ' ', $name);
+        $name = ucwords($name);
+        $name = str_replace(' ', '', $name);
+        return $name;
+    }
 
-        return parent::autoload($class);
+    /**
+     * Get existing instance of module autoloader
+     *
+     * @return ModuleAutoloader|null
+     */
+    public static function getInstance()
+    {
+        return self::$_instance;
     }
 }
 
--- library/ZeframMvc/ModuleManager/Listener/ModuleResolverListener.php
+++ library/ZeframMvc/ModuleManager/Listener/ModuleResolverListener.php
@@ -4,6 +4,7 @@ namespace ZeframMvc\ModuleManager\Listener;
 
 use Zend\ModuleManager\Listener\AbstractListener;
 use Zend\ModuleManager\ModuleEvent;
+use ZeframMvc\Loader\ModuleAutoloader;
 
 class ModuleResolverListener extends AbstractListener
 {
@@ -16,40 +17,18 @@ class ModuleResolverListener extends AbstractListener
         $moduleName = $e->getModuleName();
         $class      = $moduleName . '\Module';
 
+        // autoloading will not be triggered for invalid namespaces (PHP 5.6)
+        // we need to call autoload explicitly
         if (!class_exists($class)) {
-            // If expected module class was not found, check for the possible class name variations:
-            // - If module directory uses ZF1 naming style (spinal-case instead of CamelCase), check
-            //   for Module class in Camel-Cased module name namespace
-            // - If that fails, check for class name prefixed with Camel-Cased module name
-            //   (no autoloading involved here)
-
-            $moduleName = $this->formatModuleName($moduleName);
-            $class      = $moduleName . '\Module';
-
-            if (!class_exists($class)) {
-                $class = $moduleName . '_Module';
-                if (!class_exists($class, false)) {
-                    return false;
-                }
+            $moduleAutoloader = ModuleAutoloader::getInstance();
+            if ($moduleAutoloader && ($loadedClass = $moduleAutoloader->autoload($class))) {
+                $class = $loadedClass;
+            } else {
+                return false;
             }
         }
 
         $module = new $class;
         return $module;
-    }
-
-    /**
-     * Format a module name to the module class prefix
-     *
-     * @param  string $name
-     * @return string
-     */
-    protected function formatModuleName($name)
-    {
-        $name = strtolower($name);
-        $name = str_replace(array('-', '.'), ' ', $name);
-        $name = ucwords($name);
-        $name = str_replace(' ', '', $name);
-        return $name;
     }
 }
