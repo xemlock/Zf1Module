dd1e1cb7135e579291c3fcc9a727f7a4c34131b6
xemlock <xemlock@gmail.com>
2015-10-23 20:33:43 +0200

    throws runtime exception when service locator is not configured

--- library/ZeframMvc/Dispatcher.php
+++ library/ZeframMvc/Dispatcher.php
@@ -1,20 +1,4 @@
 <?php
-/*
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * This software consists of voluntary contributions made by many individuals
- * and is licensed under the MIT license.
- */
 
 namespace ZeframMvc;
 
@@ -27,8 +11,8 @@ use Zend_Controller_Response_Abstract as Response;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use Zend\ServiceManager\ServiceLocatorAwareInterface;
 
-class Dispatcher extends StandardDispatcher implements
-    ServiceLocatorAwareInterface
+class Dispatcher extends StandardDispatcher
+    implements ServiceLocatorAwareInterface
 {
 
     /**
@@ -55,6 +39,9 @@ class Dispatcher extends StandardDispatcher implements
      */
     public function getServiceLocator()
     {
+        if (empty($this->serviceLocator)) {
+            throw new RuntimeException('Unable to retrieve service locator');
+        }
         return $this->serviceLocator;
     }
 
@@ -201,6 +188,10 @@ class Dispatcher extends StandardDispatcher implements
         $controller = null;
     }
 
+    /**
+     * @param string $class
+     * @return string
+     */
     public function classToFilename($class)
     {
         return str_replace('_', DIRECTORY_SEPARATOR, $class) . '.php';
