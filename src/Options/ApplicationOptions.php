402d29896c5cb75b775de806055fcdf6be57fe65
xemlock <xemlock@gmail.com>
2015-11-25 10:16:29 +0100

    ApplicationOptions class handles options for Application constructor

--- src/Options/ApplicationOptions.php
+++ src/Options/ApplicationOptions.php
@@ -2,14 +2,8 @@
 
 namespace Zf1Module\Options;
 
-use Zf1Module\Exception\InvalidArgumentException;
-use Zend\Stdlib\ArrayUtils;
+use Zend\Stdlib\AbstractOptions;
 
-// this is to avoid processing of whole config array in bootstrap. Only
-// relevant keys will be provided. And also a proper order will be maintained
-// i.e. PluginLoader will be before pluginPaths
-// This options must be understandable by ZF1 Bootstrap, so key normalization
-// is different.
 class ApplicationOptions extends AbstractOptions
 {
     /**
@@ -18,84 +12,14 @@ class ApplicationOptions extends AbstractOptions
     protected $environment = 'production';
 
     /**
-     * @var string|array
+     * @var array|string|\Zend_Config
      */
-    protected $bootstrap;
+    protected $options = array();
 
     /**
-     * @var array
+     * @var bool
      */
-    protected $resources = array();
-
-    protected $pluginLoader;
-
-    /**
-     * @var array
-     */
-    protected $pluginPaths = array();
-
-    protected $resourceLoader;
-
-    /**
-     * @param array|\Traversable $resources
-     */
-    public function setResources($resources)
-    {
-        if ($resources instanceof \Traversable) {
-            $resources = ArrayUtils::iteratorToArray($resources);
-        }
-        if (!is_array($resources)) {
-            throw new InvalidArgumentException(sprintf(
-                'Parameter provided to %s must be an array or Traversable',
-                __METHOD__
-            ));
-        }
-        foreach ($resources as $key => $value) {
-            if (is_string($key) && $key !== ($normalizedKey = strtolower($key))) {
-                if (isset($resources[$normalizedKey])) {
-                    $resources[$normalizedKey] = ArrayUtils::merge(
-                        $resources[$normalizedKey],
-                        $value
-                    );
-                } else {
-                    $resources[$normalizedKey] = $value;
-                }
-            }
-        }
-        $this->resources = $resources;
-    }
-
-    /**
-     * @return array
-     */
-    public function getResources()
-    {
-        return $this->resources;
-    }
-
-    public function setPluginPaths($pluginPaths)
-    {
-        foreach ($pluginPaths as $prefix => $path) {
-            $this->pluginPaths[$prefix] = $path;
-        }
-        return $this;
-    }
-
-    public function getPluginPaths()
-    {
-        return $this->pluginPaths;
-    }
-
-    public function setBootstrap($bootstrap)
-    {
-        $this->bootstrap = $bootstrap;
-        return $this;
-    }
-
-    public function getBootstrap()
-    {
-        return $this->bootstrap;
-    }
+    protected $suppressNotFoundWarnings;
 
     /**
      * @param string $environment
@@ -112,5 +36,39 @@ class ApplicationOptions extends AbstractOptions
     public function getEnvironment()
     {
         return $this->environment;
+    }
+
+    /**
+     * @param array|string|\Zend_Config $options
+     */
+    public function setOptions($options)
+    {
+        $this->options = $options;
+        return $this;
+    }
+
+    /**
+     * @return array|string|\Zend_Config
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * @param bool $suppressNotFoundWarnings
+     */
+    public function setSuppressNotFoundWarnings($suppressNotFoundWarnings)
+    {
+        $this->suppressNotFoundWarnings = $suppressNotFoundWarnings;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getSuppressNotFoundWarnings()
+    {
+        return $this->suppressNotFoundWarnings;
     }
 }
--- src/Service/ApplicationFactory.php
+++ src/Service/ApplicationFactory.php
@@ -2,25 +2,30 @@
 
 namespace Zf1Module\Service;
 
-use Zf1Module\Application;
-use Zf1Module\Options\ApplicationOptions;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
+use Zf1Module\Application;
+use Zf1Module\Options\ApplicationOptions;
 
 class ApplicationFactory implements FactoryInterface
 {
     /**
      * Create the Bootstrap service
      *
-     * @param  ServiceLocatorInterface $serviceLocator
-     * @return Application
+     * @param  \Zend\ServiceManager\ServiceLocatorInterface $serviceLocator
+     * @return \Zf1Module\Application
      */
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
         $config = $serviceLocator->has('Config') ? $serviceLocator->get('Config') : array();
-        $options = new ApplicationOptions($config);
+        $options = new ApplicationOptions(isset($config['zf1']) ? $config['zf1'] : array());
 
-        $application = new Application($options->getEnvironment(), $options->toArray());
+        $application = new Application(
+            $options->getEnvironment(),
+            $options->getOptions(),
+            $options->getSuppressNotFoundWarnings()
+        );
+
         return $application;
     }
 }
